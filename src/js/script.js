9e2fb71f3bec7e053ec26bb1148b37650adf625f
Karol-Waliszewski <karolwaliszewski@gmail.com>
2019-11-19 14:42:23 +0100

    Added better error messages in contact form

--- src/js/script.js
+++ src/js/script.js
@@ -1,144 +1,148 @@
 // NAV and SCROLL MODULE
 
-(function () {
+(function() {
   // DOM Elements
-  var $nav = document.getElementById('nav');
-  var $burgerWrapper = document.querySelector('.nav__burger');
-  var $burger = document.getElementById('burger');
-  var $links = document.querySelectorAll('.nav__link');
-  var $scroll = document.getElementById('scroll');
+  var $nav = document.getElementById("nav");
+  var $burgerWrapper = document.querySelector(".nav__burger");
+  var $burger = document.getElementById("burger");
+  var $links = document.querySelectorAll(".nav__link");
+  var $scroll = document.getElementById("scroll");
 
   // Variables
   var active = false;
   var mobile = 768;
 
   // Functions
-  var toggleActive = function () {
+  var toggleActive = function() {
     active = !active;
-    $nav.classList.toggle('active');
-    $burger.classList.toggle('active');
-    $burgerWrapper.classList.toggle('active');
+    $nav.classList.toggle("active");
+    $burger.classList.toggle("active");
+    $burgerWrapper.classList.toggle("active");
     if (window.innerWidth < mobile) {
-      if (active)
-        $nav.addEventListener('touchmove', disableScroll, false);
-      else
-        $nav.removeEventListener('touchmove', disableScroll, false);
+      if (active) $nav.addEventListener("touchmove", disableScroll, false);
+      else $nav.removeEventListener("touchmove", disableScroll, false);
     }
   };
 
-  var disableScroll = function (e) {
+  var disableScroll = function(e) {
     e.preventDefault();
   };
 
-  var checkVisibility = function () {
+  var checkVisibility = function() {
     let offset = window.scrollY;
     let height = window.innerHeight;
     if (offset > height / 2) {
-      $scroll.classList.remove('hidden');
+      $scroll.classList.remove("hidden");
     } else {
-      $scroll.classList.add('hidden');
+      $scroll.classList.add("hidden");
     }
   };
 
-  var checkShadow = function () {
+  var checkShadow = function() {
     let offset = window.scrollY;
     let height = window.innerHeight;
     if (offset > height - $burgerWrapper.clientHeight - 1) {
-      $burgerWrapper.classList.add('shadow');
+      $burgerWrapper.classList.add("shadow");
     } else {
-      $burgerWrapper.classList.remove('shadow');
+      $burgerWrapper.classList.remove("shadow");
     }
   };
 
   // Listeners
-  window.addEventListener('scroll', checkVisibility);
-  window.addEventListener('scroll', checkShadow);
-  $burger.addEventListener('click', toggleActive);
+  window.addEventListener("scroll", checkVisibility);
+  window.addEventListener("scroll", checkShadow);
+  $burger.addEventListener("click", toggleActive);
   for (let $link of $links) {
-    $link.addEventListener('click', function () {
-      if (window.innerWidth < mobile)
-        toggleActive();
+    $link.addEventListener("click", function() {
+      if (window.innerWidth < mobile) toggleActive();
     });
   }
 
   checkVisibility();
   checkShadow();
-
 })();
 
 // CONTACT MODULE
 
-(function () {
+(function() {
   // DOM Elements
-  var $form = document.getElementById('contactForm');
-  var $name = document.getElementById('name');
-  var $email = document.getElementById('email');
-  var $message = document.getElementById('message');
-  var $submit = document.getElementById('submit');
+  var $form = document.getElementById("contactForm");
+  var $name = document.getElementById("name");
+  var $email = document.getElementById("email");
+  var $message = document.getElementById("message");
+  var $submit = document.getElementById("submit");
+  var $error = document.getElementById("submitError");
 
   // Variables
   var inputs = [$email, $name, $message];
 
   // Functions
-  var validateData = function () {
+  var validateData = function() {
     let flag = true;
 
     for (let input of inputs) {
       if (input.value.length < 1) {
-        //alert('Proszę o wypełnienie wszystkich pól');
-        input.classList.add('error');
-        input.nextElementSibling.classList.add('active');
+        input.classList.add("error");
+        input.parentNode
+          .querySelector(".contact__error")
+          .classList.add("active");
         flag = false;
       } else {
-        input.classList.remove('error');
-        input.nextElementSibling.classList.remove('active');
+        input.classList.remove("error");
+        input.parentNode
+          .querySelector(".contact__error")
+          .classList.remove("active");
       }
     }
 
-    if (!$email.value.includes('@') || !$email.value.includes('.')) {
-      $email.classList.add('error');
+    if (!$email.value.includes("@") || !$email.value.includes(".")) {
+      $email.classList.add("error");
       flag = false;
+    } else {
+      $email.classList.remove("error");
     }
 
     return flag;
   };
 
-  var clearForm = function () {
+  var clearForm = function() {
+    let errors = [...document.querySelectorAll(".contact__error")];
     $form.reset();
     for (let input of inputs) {
-      input.classList.remove('error');
-      input.nextElementSibling.classList.remove('active');
+      input.classList.remove("error");
+    }
+    for (let error of errors) {
+      error.classList.remove("active");
     }
   };
 
-  var submitForm = function (e) {
+  var submitForm = function(e) {
     e.preventDefault();
-    $submit.setAttribute('disabled', true);
+    $submit.setAttribute("disabled", true);
 
     if (validateData()) {
       sendMessage();
     } else {
-      setTimeout(function () {
+      setTimeout(function() {
         submitError();
-        $submit.removeAttribute('disabled');
+        $submit.removeAttribute("disabled");
       }, 500);
     }
-
   };
 
-  var submitSuccess = function () {
-    $submit.value = 'Pomyślnie wysłano wiadomość';
-    $submit.classList.add('success');
-    $submit.classList.remove('error');
+  var submitSuccess = function() {
+    $submit.value = "Pomyślnie wysłano wiadomość";
+    $submit.classList.add("success");
+    $submit.classList.remove("error");
   };
 
-  var submitError = function () {
-    $submit.value = 'Wyślij';
-    $submit.classList.add('error');
-    $submit.classList.remove('success');
+  var submitError = function() {
+    $submit.value = "Wyślij";
+    $submit.classList.add("error");
+    $submit.classList.remove("success");
   };
 
-  var sendMessage = function () {
+  var sendMessage = function() {
     let service_id = "lolekappa";
     let template_id = "template_da8gZu7J";
 
@@ -152,25 +156,27 @@
       () => {
         submitSuccess();
         clearForm();
-        $submit.removeAttribute('disabled');
+        $submit.removeAttribute("disabled");
+        $error.classList.remove("active");
       },
       err => {
         console.error(err);
         submitError();
-        $submit.removeAttribute('disabled');
-      });
+        $error.classList.add("active");
+        $submit.removeAttribute("disabled");
+      }
+    );
   };
 
   // Listener
-  $form.addEventListener('submit', submitForm);
-
+  $form.addEventListener("submit", submitForm);
 })();
 
-// VH variable mobile fix 
+// VH variable mobile fix
 function fixVH() {
   let vh = document.documentElement.clientHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
 
 fixVH();
-window.addEventListener('resize', fixVH);
+window.addEventListener("resize", fixVH);
